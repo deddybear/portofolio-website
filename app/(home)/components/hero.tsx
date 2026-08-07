18a59a01f78be1700d6aa8744af21c04e7fbbf55
deddybear <dedi.suharman05@gmail.com>
2026-08-07 21:40:41 +0700

    proses hero

--- app/(home)/components/hero.tsx
+++ app/(home)/components/hero.tsx
@@ -1,13 +1,36 @@
 export default function HeroSection() {
     return (
-        <div className="container">
-            <div className="flex flex-wrap">
-                <div className="w-full self-center px-4">
-                    <h1 className="text-base">Hallo 👋, lorem ipsum</h1>
-                    <h2>Lorem Ipsum Dolor si amet</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+        <div className="">
+            {/* Introduction Self Start */}
+            <div className="flex mb-10">
+                <div className="w-full self-center px-2">
+                    <h1 className="font-sans font-bold text-3xl tracking-widest mb-1">Hello, I&apos;am Dedi,</h1>
+                    <h1 className="font-sans font-semibold text-2xl tracking-wider mb-1">Fullstack Developer</h1>
+                    <p className="font-sans lg:max-w-2xl font-normal text tracking-wide mb-1">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eligendi totam unde voluptatem,
+                        officia quas? Eveniet tenetur libero officiis, corrupti neque ex nostrum id, voluptates, dolorum sunt odio magnam? Cum?
+                    </p>
+                </div>
+                <div className="hidden lg:flex lg:w-1/2">
+                    <h1 className="">ini gambar</h1>
                 </div>
             </div>
+            {/* Introduction Self End */}
+
+            <div className="px-2">
+                <div className="flex flow-row">
+                    {/* Button Download Resume Start */}
+                    <div className="font-sans container">
+                        <button className="bg-linear-to-r from-[#00B4DB] to-[#5B86E5] text-white font-bold py-2 px-4 rounded">Download Resume</button>
+                    </div>
+                    {/* Button Download Resume End */}
+
+                </div>
+            </div>
+
+
+
+
         </div>
     )
-}
+}   
--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -4,40 +4,38 @@ export default function NavbarComponent() {
 
     return (
         <nav className="relative after:pointer-events-none after:absolute">
-            <div className="mx-auto">
-                <div className="relative flex items-center justify-between border-3 border-blue-500">
-                    {/* Mobile Menu Button Start */}
-                    <div className="absolute inset-y-0 right-0 flex items-center sm:hidden border-2 border-red-500">
-                        <button type="button" className="relative inline-flex items-center justify-center">
-                            <span className="absolute -inset-0.5"></span>
-                            <span className="sr-only">Main Menu</span>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
-                                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 not-in-aria-expanded:hidden">
-                                <path d="M6 18 18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
-                    </div>
-                    {/* Mobile Menu Button End */}
-
-                    {/* Logo Webiste Start */}
-                    <div className="flex flex-1 items-center sm:items-stretch sm:justify-start border-2 border-red-500">
-                        <a href="#" className="block py-6 font-bold">Ini Logo</a>
-                    </div>
-                    {/* Logo Webiste End */}
-
-                    {/* Item Navbar Start */}
-                    <div className="hidden sm:block border-2 border-red-500">
-                        <div className="flex space-x-4">
-                            <a href="#home" className="px-3 py-2 text-sm font-medium">Home</a>
-                            <a href="#about" className="px-3 py-2 text-sm font-medium">About</a>
-                            <a href="#projects" className="px-3 py-2 text-sm font-medium">Portfolio</a>
-                            <a href="#contact" className="px-3 py-2 text-sm font-medium">Contact</a>
-                        </div>
-                    </div>
-                    {/* Item Navbar End */}
+            <div className="relative flex items-center justify-between px-1 lg:px-0">
+                {/* Mobile Menu Button Start */}
+                <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                    <button type="button" className="relative inline-flex items-center justify-center">
+                        <span className="absolute inset-0.5"></span>
+                        <span className="sr-only">Main Menu</span>
+                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
+                            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 not-in-aria-expanded:hidden">
+                            <path d="M6 18 18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </button>
                 </div>
+                {/* Mobile Menu Button End */}
+
+                {/* Logo Webiste Start */}
+                <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
+                    <a href="#" className="block py-6 font-bold">Ini Logo</a>
+                </div>
+                {/* Logo Webiste End */}
+
+                {/* Item Navbar Start */}
+                <div className="hidden sm:block">
+                    <div className="flex space-x-4">
+                        <a href="#home" className="px-3 py-2 text-sm font-medium">Home</a>
+                        <a href="#about" className="px-3 py-2 text-sm font-medium">About</a>
+                        <a href="#projects" className="px-3 py-2 text-sm font-medium">Portfolio</a>
+                        <a href="#Stack" className="px-3 py-2 text-sm font-medium">Stack</a>
+                    </div>
+                </div>
+                {/* Item Navbar End */}
             </div>
         </nav>
 
